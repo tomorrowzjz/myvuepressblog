8942ce3ca6ae1ca52135ab46d76b087387d94713
tomorrowzjz <1047547064@qq.com>
2018-12-27 16:24:24 +0800

    'myblog搭建'

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  title: 'myblog',
+  description : '666666',
+  // base : '/zhangjianzhong/',
+  themeConfig : {
+    nav : [
+      { text: '待定', link: '#' },
+      { text: 'aboutme', link: '#' },
+      { text: 'github', link: '#' }
+    ],
+    sidebar: {
+      '/' : [
+        "/",
+        "html",
+        "css",
+        "error"
+      ]
+    },
+    sidebarDepth : 2
+  }
+}
